476f13a301850684a3b44998fb63383ff51c2c37
Vijay Surla <foaksolutions@gmail.com>
2025-02-02 19:18:04 -0800

    Fix environment variables and deploy working version to all URLs

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -31,8 +31,8 @@ import TipSheet from "./tips/TipSheet"
 
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
-//console.log('API_URL:', process.env.REACT_APP_API_URL);
-//console.log('apiUrl:', apiUrl);
+console.log('API_URL:', process.env.REACT_APP_API_URL);
+console.log('apiUrl:', apiUrl);
   //console.log('NODE_ENV:', process.env.NODE_ENV);
   //console.log('All env variables:', process.env);
 
